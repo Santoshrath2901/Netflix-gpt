b7b26a9ea56e8449eeadc95fac63b9e44e78a740
Santosh Rath <santosh.rath29@gmail.com>
2024-01-16 11:53:52 +0530

    made the website responsive

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,16 +1,21 @@
 import { BG_URL } from "../utils/constants";
 import GptMovieSuggestions from "./GptMovieSuggestions";
 import GptSearchBar from "./GptSearchBar";
+import GptText from "./GptText";
 
 const GPTSearch = () => {
   return (
-    <div>
-      <div className="absolute -z-10">
-        <img src={BG_URL} alt="logo" />
+    <>
+      <div className="fixed -z-10">
+        <img className="h-screen object-cover" src={BG_URL} alt="logo" />
       </div>
-      <GptSearchBar />
-      <GptMovieSuggestions />
-    </div>
+      <div className="">
+        <GptSearchBar />
+        <GptMovieSuggestions />
+        <GptText/>
+       </div>
+    </>
+    
   );
 };
 export default GPTSearch;
---
+++ src/components/GptText.js
@@ -0,0 +1,26 @@
+const GptText = () => {
+
+  return(
+    <div class="w-96 bg-white shadow rounded-lg border-red-950 pt-6 md:p-8 text-center   ">
+    <blockquote>
+      <p class="text-lg font-medium">
+        On clicking search please wait a few seconds. On search if it shows 
+        <p class="italic">
+        "You exceeded your current plan quota, please check your plan and billing details".</p>
+        Kindly inform me. The open ai api used in this is a paid version api that helps Netflix to use the powers of Gpt.
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        Santosh Kumar Rath
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        React Devloper
+      </div>
+    </figcaption>
+  </div>
+  )  
+}
+
+export default GptText;
+
